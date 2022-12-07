81e9a46c9810de8c7318bb00af0de9f5f19f0942
Milan <milanpanda4425@gmail.com>
2022-12-07 22:21:47 +0530

    cors

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const billRoute = require("./routes/bills");
 connection();
 
 var corsOptions = {
-    origin: 'http://localhost:4200',
+    origin: '*',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
 //middlewares
